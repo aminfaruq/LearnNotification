47d36e0e4c3380569b8259a406aa664eb997045b
Faruk Amin <aminfaruk.fa@gmail.com>
2020-06-08 20:43:32 +0800

    use pending intent

--- app/src/main/java/co/id/simplenotif/MainActivity.kt
+++ app/src/main/java/co/id/simplenotif/MainActivity.kt
@@ -2,11 +2,15 @@ package co.id.simplenotif
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
@@ -21,9 +25,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    //aksi untuk onClick pada button
+    fun sendNotification(view: View) {
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"))
+        val pendingIntent = PendingIntent.getActivity(this , 0 , intent, 0)
+
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
                 .setSmallIcon(R.drawable.ic_notifications)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_notifications))
                 .setContentTitle(resources.getString(R.string.content_title))
@@ -38,7 +52,7 @@ class MainActivity : AppCompatActivity() {
 
             val channel = NotificationChannel(CHANNEL_ID,
                     CHANNEL_NAME,
-            NotificationManager.IMPORTANCE_DEFAULT)
+                    NotificationManager.IMPORTANCE_DEFAULT)
             channel.description = CHANNEL_NAME.toString()
             mBuilder.setChannelId(CHANNEL_ID)
 
